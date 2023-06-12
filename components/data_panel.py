43ecd6991589813011f7573fd5ed18652fee2292
Justin Elms <justin.elms@dfo-mpo.gc.ca>
2023-06-12 09:00:53 -0230

    add loading panel

--- components/data_panel.py
+++ components/data_panel.py
@@ -8,10 +8,13 @@ from PySide6.QtWidgets import (
 import numpy as np
 
 from data.dataset import Dataset
+from components.loading_panel import LoadingPanel
 
 
 class DataPanel(QWidget):
-    resize_panel = Signal(int)
+    resize_header = Signal(int)
+    resize_spinner = Signal(int, int)
+    loading = Signal(bool)
 
     def __init__(
         self,
@@ -33,6 +36,12 @@ class DataPanel(QWidget):
         )
         self.csv_data = self.dataset.config["csv_data"]
 
+        self.loading_panel = LoadingPanel(self)
+        self.loading_panel.raise_()
+        self.resize_spinner.connect(self.loading_panel.resize_panel)
+        self.loading.connect(self.set_loading)
+
+        self.setFixedWidth(120)
         self.layout = QVBoxLayout(self)
         self.layout.setSpacing(0)
         self.layout.setContentsMargins(0, 0, 0, 0)
@@ -42,8 +51,16 @@ class DataPanel(QWidget):
     def close_panel(self) -> None:
         self.deleteLater()
 
+    @Slot(bool)
+    def set_loading(self, show_spinner) -> None:
+        if show_spinner:
+            self.loading_panel.show()
+        else:
+            self.loading_panel.hide()
+
     def resizeEvent(self, event: QResizeEvent) -> None:
-        self.resize_panel.emit(self.geometry().width())
+        self.resize_header.emit(self.geometry().width())
+        self.resize_spinner.emit(self.width, self.geometry().height())
 
     def hex_to_rgb(self, hex):
         return np.array([int(hex[i: i + 2], 16) for i in (0, 2, 4)])
---
+++ components/loading_panel.py
@@ -0,0 +1,30 @@
+from PySide6.QtCore import Qt, Slot
+from PySide6.QtGui import QMovie
+from PySide6.QtWidgets import (
+    QLabel,
+    QVBoxLayout,
+    QWidget,
+)
+
+
+class LoadingPanel(QWidget):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent=parent)
+
+        spinner = QMovie(":/loading.gif")
+
+        loading_label = QLabel(self)
+        loading_label.setMovie(spinner)
+
+        spinner.start()
+
+        self.layout = QVBoxLayout(self)
+        self.layout.setAlignment(Qt.AlignCenter)
+        self.layout.addStretch()
+        self.layout.addWidget(loading_label)
+        self.layout.addStretch()
+        self.setLayout(self.layout)
+
+    @Slot(int, int)
+    def resize_panel(self, width: int, height: int) -> None:
+        self.setFixedSize(width, height)
